8aa3786ba5657ba253e4a22c288b298ad81db9d7
Pratik <karkipratik063@gmail.com>
2025-06-30 12:27:06 +0545

    added route for about

--- worldclock/src/App.tsx
+++ worldclock/src/App.tsx
@@ -7,6 +7,7 @@ import Home from './components/Home';
 import Timed from './components/Timed.jsx';
 import Sec from './components/Sec.jsx';
 import Min from './components/Min';
+import About from './components/About';
 import { Route, Routes } from 'react-router-dom';
 
 
@@ -19,6 +20,7 @@ function App() {
       <Route path="/timed" element={<Timed />} />
       <Route path="/Sec" element={<Sec />} />
       <Route path="/Min" element={<Min />} />
+      <Route path="/About" element={<About />} />
     </Routes>
   );
 }
